615157a7cd112496fa05aa2303c887739e95edc9
UlianaVicol <75852790+UlianaVicol@users.noreply.github.com>
2022-03-20 15:59:42 +0200

    Add files via upload

---
+++ naming.py
@@ -0,0 +1,11 @@
+students_firstname = "John"
+students_lastname  = "Doe" # students lastname: denumirile compuse sunt separate vizual
+student_age        = 25
+students_country   = "Canada"
+students_town      = "Ottawa"
+
+print( 'Numele Studentului:', students_firstname )
+print( 'Prenumele Studentului:', students_lastname )
+print( ' Varsta Studentului:', student_age )
+print( 'Tara de Provenienta:', students_country )
+print( 'Orasul de Bastina:', students_town )
