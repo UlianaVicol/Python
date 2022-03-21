cc37f3df94bb71291383b060bd5de2ee1a6103d8
UlianaVicol <75852790+UlianaVicol@users.noreply.github.com>
2022-03-21 11:18:04 +0200

    Add files via upload

---
+++ multiple_variables_naming_hw.py
@@ -0,0 +1,8 @@
+
+students_firstname, students_lastname, student_age, students_country, students_town  = "John", "Doe", 15, "Canada", "Ottawa"
+
+print(  students_firstname )
+print( students_lastname )
+print( student_age )
+print( students_country )
+print( students_town )
