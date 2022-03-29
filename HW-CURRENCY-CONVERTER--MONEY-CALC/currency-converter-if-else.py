4494310ec053b0e1fbcb702ede32b436801718b8
UlianaVicol <75852790+UlianaVicol@users.noreply.github.com>
2022-03-29 14:30:49 +0300

    Update currency-converter-if-else.py

--- HW-CURRENCY-CONVERTER--MONEY-CALC/currency-converter-if-else.py
+++ HW-CURRENCY-CONVERTER--MONEY-CALC/currency-converter-if-else.py
@@ -9,6 +9,7 @@ print(" * EUR")
 print(" * USD")
 print(" * MDL")
 print("##########################")
+
 currency_in = input("choose: ")
 money_in = float(input("money: "))
 
@@ -21,18 +22,23 @@ if currency_in != "USD":
     print(" * USD")
 if currency_in != "MDL":
     print(" * MDL")
+
 currency_out = input("choose: ")
+
 eur_2_usd = 1.1
 eur_2_mdl = 20.0
-if currency_in == "EUR" and  currency_out == "USD":
-    money_out = money_in * eur_2_usd
-if currency_out == "MDL":
-    money_out = money_in * eur_2_mdl
 
-if currency_in == "USD" and currency_out == "EUR":
-    money_out = money_in / eur_2_usd    
-if currency_out == "MDL":
-    money_out = money_in * eur_2_mdl / eur_2_usd
+if currency_in == "EUR":
+    if currency_out == "USD":
+        money_out = money_in * eur_2_usd
+    if currency_out == "MDL":
+        money_out = money_in * eur_2_mdl 
+
+if currency_in == "USD":
+    if currency_out == "EUR":
+        money_out = money_in / eur_2_usd    
+    if currency_out == "MDL":
+        money_out = money_in * eur_2_mdl / eur_2_usd
 
 if currency_in == "MDL":
     if currency_out == "EUR":
