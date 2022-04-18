9c7004ae5115015c1c37aa7670fd277e5a9c59de
UlianaVicol <75852790+UlianaVicol@users.noreply.github.com>
2022-04-18 14:55:07 +0300

    Add files via upload

---
+++ HW-ANALOGIC-WATCH-ALARM/clock.py
@@ -0,0 +1,52 @@
+# clock module
+import turtle as t
+t.bgcolor("black")
+c_radius = 300
+
+#print the time
+def printTime(hours, minutes, seconds, amPm):
+    print( f"{hours:02}:{minutes:02}:{seconds:02} {amPm}" )
+
+def drawClock(hours, minutes, seconds):
+    t.clear()
+    drawClockCircle()
+    drawClockHours(hours)
+    drawClockMinutes(minutes)
+    drawClockSeconds(seconds)
+
+def drawClockCircle():
+    # the circle
+    t.speed(0)
+    t.penup()
+    t.color("DarkTurquoise")
+    t.setheading(0)
+    t.setposition(0, -c_radius)
+    t.pendown()
+    t.circle(c_radius)
+
+def drawClockHours(hours):
+    t.penup()
+    t.color("white")
+    t.setposition(0,0)
+    t.pensize(5)
+    t.pendown()
+    t.setheading(90 + hours * -30)
+    t.forward(c_radius * 0.7)
+
+def drawClockMinutes(minutes):
+    t.penup()
+    t.color("yellow")
+    t.setposition(0, 0)
+    t.pensize(2)
+    t.pendown()
+    t.setheading(90 + minutes * -6)
+    t.forward(c_radius * 0.8)
+
+def drawClockSeconds(seconds):
+    t.penup()
+    t.color("dark red")
+    t.setposition(0, 0)
+    t.pensize(1)
+    t.pendown()
+    t.setheading(90 + seconds * -6)
+    t.forward(c_radius * 0.9)
